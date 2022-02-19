140336adaa98bdc2b4c718a2885aded715581709
johan <>
2022-02-19 18:04:50 +0000

    Added backup script
    FossilOrigin-Name: 4495313593bb0b851dc4e4122e50b7d60ffbe31c328503881e465d34dc917780

---
+++ scripts/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ---------------------------------------------
+# Simple script to backup a Fossil repository
+# Usage: backup.sh fossil.fossil /home/backup
+# ---------------------------------------------
+echo "Backup repos: $1 --> $2/$1"
+#docker container exec fossil fossil backup --overwrite -R $1 /data/backup
+
+VOLUME="$(docker volume inspect --format '{{ .Mountpoint }}' fossil_fossil-volume)"
+
+cp $VOLUME/$1 $2/$1

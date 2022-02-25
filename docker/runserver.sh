c853cf2215dba7f4ee1e091006cb95f46c3ccfbb
johan <>
2022-02-25 16:02:50 +0000

    Fossil 2.18
    FossilOrigin-Name: 82f5db47f16a04d06a584ccd411ea492c2d81f2038141d19767c61864d187249

--- docker/runserver.sh
+++ docker/runserver.sh
@@ -7,11 +7,18 @@ MULTI_REPO_PARAM=--repolist
 PROJECT_NAME_PARAM=--project-name
 PROJECT_DESC_PARAM=--project-desc
 
-# Configurable parameter
-ENV START_PAGE=
-ENV HTTPS_PROXY=
-ENV MULTI_REPO=
-ENV SINGLE_REPO=1
+# //////////////////////////////////////////////////////////
+# Configurable parameters
+# REPOS        - Repository (default: fossil.fossil)
+# PASSWORD     - password (default: admin1234)
+# USERNAME     - username (admin: default)
+# PROJECT_NAME - project name for a new repos
+# PROJECT_DESC - project description for a new repos
+# START_PAGE   - startpage for a new repos
+# HTTPS_PROXY  - enable https proxy
+# MULTI_REPO   - support for multiple repos
+# //////////////////////////////////////////////////////////
+
 
 # Set default values
 if [ -z ${REPOS} ] ; then
@@ -26,16 +33,22 @@ if [ -z ${PASSWORD} ] ; then
     PASSWORD="admin1234"
 fi
 
+if [ ! -z ${PROJECT_NAME} ] ; then
+    PROJECT_NAME_PARAM="${PROJECT_NAME_PARAM} ${PROJECT_NAME}"
+fi
+
+if [ ! -z ${PROJECT_DESC} ] ; then
+    PROJECT_DESC_PARAM="${PROJECT_DESC_PARAM} ${PROJECT_DESC}"
+fi
+
 # Setup new repository
 if [ ! -e ${REPO_PATH}/${REPOS} ] ; then
     NEW_REPOS="true"
-    echo "${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}"
-    ${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}
+    ${FOSSIL} init -A ${USERNAME} ${PROJECT_NAME:+$PROJECT_NAME_PARAM} ${PROJECT_DESC:+$PROJECT_DESC_PARAM} ${REPO_PATH}/${REPOS}
 fi
 
 # Change password (first time)
 if [ ! -z ${NEW_REPOS} ] &&  [ ! -z ${PASSWORD} ] ; then
-    echo "${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}"
     ${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}
 fi
 
@@ -44,11 +57,9 @@ unset $PASSWORD
 
 # Start up fossil
 if [ -e ${REPO_PATH}/${REPOS} ] ; then
-    if [ ! -z ${MULTI_REPO} ] ; then
-        echo "${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
+    if [ -z ${MULTI_REPO} ] ; then
         ${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}
     else
-        echo "${FOSSIL} server ${MULTI_REPO_PARAM} ${REPO_PATH} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
         ${FOSSIL} server ${MULTI_REPO_PARAM} ${REPO_PATH} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}
     fi
 fi

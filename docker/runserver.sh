ea9f2ebcd37d578fdd4d65fc326e9b692f623782
johan <>
2022-02-25 14:11:39 +0000

    Working initail version of runserver script
    FossilOrigin-Name: 9dbb7a3ff2171f75998054d37071fabb138e9642b4874c497271454fed38f0b6

--- docker/runserver.sh
+++ docker/runserver.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-if [ ! -z ${REPOS} ]
-then
+if [ -z ${REPOS} ] ; then
     REPOS="fossil.fossil"
 fi
 
-if [ ! -e “${REPO_PATH}/${REPOS}” ]
-then
+if [ ! -e ${REPO_PATH}/${REPOS} ] ; then
     echo "${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}"
     ${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}
 fi
 
-if [ ! -z ${PASSWORD} ]
-then
+if [ ! -z ${PASSWORD} ] ; then
     echo "${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}"
     ${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}
 fi
@@ -20,13 +17,14 @@ fi
 unset $USERNAME
 unset $PASSWORD
 
-if [ -w “/data/repos/$REPOS” ]
-then
-    if [ ! -z $MULTI_REPO ]	
+if [ -e ${REPO_PATH}/${REPOS} ] ; then
+    echo "starting..."
+    if [ ! -z ${MULTI_REPO} ] ; then
         echo "${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
         ${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT}
     else
-        echo "${FOSSIL} server --repolist $REPO_PATH --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
-        ${FOSSIL} server --repolist $REPO_PATH --port ${PORT}
+        echo "${FOSSIL} server --repolist ${REPO_PATH} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
+        ${FOSSIL} server --repolist ${REPO_PATH} --port ${PORT}
     fi
 fi
+

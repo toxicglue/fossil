e13772260b013faa729781578f867090d8ff343c
johan <>
2022-02-25 14:38:45 +0000

    Modified params
    FossilOrigin-Name: 928b151c1de33fcc76c4d2382d33026f8f50b7690b0b10b22bf8c7b928eae87d

--- docker/runserver.sh
+++ docker/runserver.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+
+# Set default values
 if [ -z ${REPOS} ] ; then
     REPOS="fossil.fossil"
 fi
 
+if [ -z ${USERNAME} ] ; then
+    USERNAME="admin"
+fi
+
+if [ -z ${PASSWORD} ] ; then
+    PASSWORD="admin1234"
+fi
+
+# Setup new repository
 if [ ! -e ${REPO_PATH}/${REPOS} ] ; then
+    NEW_REPOS="true"
     echo "${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}"
     ${FOSSIL} init -A ${USERNAME} ${REPO_PATH}/${REPOS}
 fi
 
-if [ ! -z ${PASSWORD} ] ; then
+# Change password (first time)
+if [ ! -z ${NEW_REPOS} ] &&  [ ! -z ${PASSWORD} ] ; then
     echo "${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}"
     ${FOSSIL} user password ${USERNAME} ${PASSWORD} -R ${REPO_PATH}/${REPOS}
 fi
@@ -17,8 +30,8 @@ fi
 unset $USERNAME
 unset $PASSWORD
 
+# Start up fossil
 if [ -e ${REPO_PATH}/${REPOS} ] ; then
-    echo "starting..."
     if [ ! -z ${MULTI_REPO} ] ; then
         echo "${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT} ${HTTPS_PROXY:+$HTTPS_PROXY_PARAM}"
         ${FOSSIL} server ${REPO_PATH}/${REPOS} --port ${PORT}
